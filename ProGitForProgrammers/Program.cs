d3495f6ca7e1e2e7e60eaf8f85ae0225b1b67165
Y.H.LEE <vespaleemac@gmail.com>
2025-02-19 09:32:19 +0800

    my message

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, C#!");
+Console.WriteLine("This line add to show the staging area!");
